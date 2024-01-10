9d4254fc1c75117e9664feffe8124d17778de7dc
azzahraayudita <141887644+azzahraayudita@users.noreply.github.com>
2024-01-10 12:29:36 +0700

    Add files via upload

---
+++ UjiBus.java
@@ -0,0 +1,19 @@
+package Enkapsulasi;
+
+public class UjiBus {
+    public static void main(String[] args) {
+        Bus busMini=new Bus();
+        busMini.penumpang=5;
+        busMini.maxPenumpang=5;
+        busMini.cetak();
+
+        busMini.penumpang=busMini.penumpang+5;
+        busMini.cetak();
+
+        busMini.penumpang=busMini.penumpang-2;
+        busMini.cetak();
+
+        busMini.penumpang=busMini.penumpang+8;
+        busMini.cetak();
+    }
+}
